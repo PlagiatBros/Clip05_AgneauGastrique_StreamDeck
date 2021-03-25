75bee164e19d968563e63bfc5e63ddceabdd84fd
bordun <jean-emmanuel.doucet@groolot.net>
2021-03-25 10:57:26 +0100

    debug mode

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ var http = require('http'),
     fs= require('fs'),
     path = require('path'),
     server = http.createServer((req, res)=>{
-        var url = req.url
+        var url = req.url.replace(/\?.*$/, '')
         if (url == '/') url = __dirname + '/index.html'
         else if (url.match(/.*\.(png|gif|jpg|jpeg)/i)) url = path.join(__dirname, '/pix/', url)
         else url = __dirname + url
-        console.log(url)
-        send(req, url).on('error', console.error).pipe(res)
+        if (fs.existsSync(url)) {
+            send(req, url).on('error', console.error).pipe(res)
+        } else {
+            res.writeHead(404)
+            res.end()
+            console.error('File not found: ' + url)
+        }
     })
 
 server.listen(8000)
